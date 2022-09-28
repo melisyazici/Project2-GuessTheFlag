27eeccbe6f17a05cc909c73698f509177958b4e4
Melis Yazıcı <melis.yazici95@icloud.com>
2022-09-28 20:45:35 +0200

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Melis Yazıcı on 28.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
